00c7a466dc38ca09fdeaaaa4535049cb78bfaac0
张博 <zhangbo21@baidu.com>
2015-09-09 00:53:59 +0800

    release to solve font-family bu9g

--- hotbox.min.js
+++ hotbox.min.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * Hot Box UI - v0.1.0 - 2015-09-08
+ * Hot Box UI - v1.0.11 - 2015-09-09
  * https://github.com/fex-team/hotbox
  * GitHub: https://github.com/fex-team/hotbox.git 
  * Copyright (c) 2015 Baidu FEX; Licensed BSD
